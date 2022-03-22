2e17fe1107b34070bb647c913a0a22b4d1173168
JungMJ322 <96896783+JungMJ322@users.noreply.github.com>
2022-03-22 16:08:42 +0900

    Create subwayPertreat.py
    역사정보.xlsx 전처리 함수
    xlsxToJson.py의 함수를 사용하여 xlsx을 json으로 변경

---
+++ module/subwayPertreat.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import json
+import getLocation
+import xlsxToJson
+
+location = './json/'
+file_name = 'subway.json'
+json_name = 'subway2.json'
+
+def subwayPer(location=location, file_name=file_name, json_name=json_name):
+    with open((location + file_name), 'r', encoding='utf8') as f:
+        json_file = json.load(f)
+
+    df = pd.DataFrame(json_file['data'])
+    subway = df.loc[:,['역번호', '역사명', '노선명', '역위도', '역경도', '역사 도로명주소']]
+    subway.columns  = ['subway_code', 'stn_name', 'route_name', 'lat', 'lon', 'road_add']
+
+    json_data = subway.to_json(location+json_name, orient = 'table', indent=4, force_ascii=False)
+
+    with open((location+json_name), 'r', encoding='utf8') as f:
+        json_file = json.load(f)
+
+    for data in json_file['data']:
+        if data['lon'] == None:
+            loca_dict = getLocation.kakao_location(data['road_add'])
+            data['lon'] = loca_dict['lon']
+            data['lat'] = loca_dict['lat']
+
+    with open((location + json_name), 'w', encoding='utf8') as f:
+        json.dump(json_file, f, indent=4, ensure_ascii=False)
+
+if __name__ == '__main__':
+    xlsxToJson.getXlsxToJson()
+    subwayPer()
