9fbf480a09021bce9e51dbaf17191de7b752a434
sol <soloolol@naver.com>
2022-03-24 05:58:49 +0900

    hosts open
    배포할때 다들어오게 열어주기

--- django/housing/housing/settings.py
+++ django/housing/housing/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=*^z_x(d7am_)z)*rbm-4)vmc6&!p8!dyig5xfsv1%yi&bob%(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
