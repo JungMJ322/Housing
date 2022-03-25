07d83f414e5aaede5f375fb05ba4fa052918d7bb
JungMJ322 <96896783+JungMJ322@users.noreply.github.com>
2022-03-25 16:08:19 +0900

    Update subwayPertreat.py
    최종

--- data/change_module/subwayPertreat.py
+++ data/change_module/subwayPertreat.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import json
 import getLocation
+from pyspark.sql.functions import col
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.master('local[1]').appName('getInfra').getOrCreate()
@@ -11,28 +12,36 @@ json_name = 'subway2.json'
 
 def subwayPer(location=location, file_name=file_name, json_name=json_name):
 
-    df_json = spark.read.json(f'/Housing/data/hadoop_upload/{file_name}')
+    df_json = spark.read.json('/housing/data/'+file_name, encoding='utf8')
     df_json.createOrReplaceTempView('subway')
-    df_json2 = spark.sql("""select 역번호 as subway_code, 역사명 as stn_name, 노선명 as route_name, 
-                            역위도 as lat, 역경도 as lot 역사 도로명주소 as place form subway""")
     
-    df_json3 = df_json2.collect()
+    df_json3 = df_json.collect()
 
     json_file = list()
     for doc in df_json3:
         json_file.append(doc.asDict())
 
     cnt = 0
+    json_file2 = list()
     for data in json_file:
-        if data['lot'] == None:
-            loca_dict = getLocation.kakao_location(data['place'])
-            data['lot'] = loca_dict['lot']
-            data['lat'] = loca_dict['lat']
-        data['id'] = cnt
+        json_dict = dict()
+        json_dict['id'] = cnt
+        json_dict['subway_code'] = data['역번호']
+        json_dict['stn_name'] = data['역사명']
+        json_dict['route_name'] = data['노선명']
+        if data['역경도'] == None:
+            loca_dict = getLocation.kakao_location(data['역사 도로명주소'])
+            json_dict['lot'] = loca_dict['lot']
+            json_dict['lat'] = loca_dict['lat']
+        else:
+            json_dict['lat'] = data['역위도']
+            json_dict['lot'] = data['역경도']
+        json_dict['place'] = data['역사 도로명주소']
         cnt = cnt + 1
+        json_file2.append(json_dict)
 
-    with open((location + file_name), 'w', encoding='utf8') as f:
-        json.dump(json_file['data'], f, ensure_ascii=False)
+    with open((location + json_name), 'w', encoding='utf8') as f:
+        json.dump(json_file2, f, ensure_ascii=False)
 
 if __name__ == '__main__':
     subwayPer()
