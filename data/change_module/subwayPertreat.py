b0f47b63e3d6d195ee5df79085d517c10c17e842
JungMJ322 <96896783+JungMJ322@users.noreply.github.com>
2022-03-25 15:12:40 +0900

    Update subwayPertreat.py

--- data/change_module/subwayPertreat.py
+++ data/change_module/subwayPertreat.py
@@ -2,34 +2,35 @@ import pandas as pd
 import json
 import getLocation
 import xlsxToJson
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.master('local[1]').appName('getInfra').getOrCreate()
 
 location = '../data/json/'
 file_name = 'subway.json'
 json_name = 'subway2.json'
 
 def subwayPer(location=location, file_name=file_name, json_name=json_name):
-    xlsxToJson.getXlsxToJson()
 
-    with open((location + file_name), 'r', encoding='utf8') as f:
-        json_file = json.load(f)
+    df_json = spark.read.json(f'/Housing/data/hadoop_upload/{file_name}.json')
+    df_json.createOrReplaceTempView('subway')
+    df_json2 = spark.sql("""select 역번호 as subway_code, 역사명 as stn_name, 노선명 as route_name, 
+                            역위도 as lat, 역경도 as lot 역사 도로명주소 as place form subway""")
+    
+    df_json3 = df_json2.collect()
 
-    df = pd.DataFrame(json_file['data'])
-    subway = df.loc[:,['역번호', '역사명', '노선명', '역위도', '역경도', '역사 도로명주소']]
-    subway.columns  = ['subway_code', 'stn_name', 'route_name', 'lat', 'lot', 'place']
-
-    json_data = subway.to_json(location+json_name, orient = 'table', indent=4, force_ascii=False)
-
-    with open((location+json_name), 'r', encoding='utf8') as f:
-        json_file = json.load(f)
+    json_file = list()
+    for doc in df_json3:
+        json_file.append(doc.asDict())
 
     cnt = 0
-    for data in json_file['data']:
+    for data in json_file:
         if data['lot'] == None:
             loca_dict = getLocation.kakao_location(data['place'])
-            data['id'] = cnt
             data['lot'] = loca_dict['lot']
             data['lat'] = loca_dict['lat']
-            cnt = cnt + 1
+        data['id'] = cnt
+        cnt = cnt + 1
 
     with open((location + json_name), 'w', encoding='utf8') as f:
         json.dump(json_file['data'], f, ensure_ascii=False)
