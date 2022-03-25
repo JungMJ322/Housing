798d2b8b5b26f9bb28656f518dff2b50cb6cccda
BubDubJP <96857362+BubDubJP@users.noreply.github.com>
2022-03-25 16:31:50 +0900

    sold_cost_data_Added

--- data/change_module/main_test.py
+++ data/change_module/main_test.py
@@ -9,7 +9,5 @@ if __name__ == "__main__":
     convinient_change.mart_change()
     convinient_change.park_change()
     convinient_change.bus_change()
-
     competition_change.compet_change()
-
     detail_change.change_detail()
--- data/change_module/sold_cost_change.py
+++ data/change_module/sold_cost_change.py
@@ -1,114 +1,94 @@
 import csv
 import json
+from pyspark.sql import SparkSession
 
+spark = SparkSession.builder.master('local[1]').appName('sold_cost').getOrCreate()
 
 
 def create_code_dict():
-    with open('../data/sold_cost/area_code.txt', 'r', encoding='euc-kr') as f:
+    with open('../data/hadoop_upload/sold_cost/area_code.txt', 'r', encoding='euc-kr') as f:
         rdr = csv.reader(f, delimiter='\t')
-        count = 0
         code_dict = {}
+
         for row in rdr:
             code_dict[row[1]] = row[0]
+
         return code_dict
 
 
-
 def save_json():
+    temp_list = []
+
     for i in range(1, 13):
-        # with open("../data/sold_cost/20." + str(i) + ".csv", "r", encoding='euc-kr') as f:
-        #     rdr = csv.reader(f)
-        data = spark.read.json("/Housing/data/hadoop_upload/20."+str(i)+".csv", encoding="cp949", header=True)
+        data = spark.read.csv("/Housing/data/hadoop_upload/sold_cost/21."+str(i)+".csv", encoding="cp949")
         data_coll = data.collect()
         rdr = list()
+
+        for j in data_coll:
+            rdr.append(j.asDict())
+        code_dict = create_code_dict()
+
+        for j in rdr:
+            temp = dict()
+            temp['place'] = j[0]
+            temp['area_grade'] = str(int(float(j[5]) // 10)) + '단위'
+            try:
+                temp['area_code'] = code_dict[j[0]]
+            except KeyError:
+                continue
+            temp['month'] = j[6]
+            temp['cost'] = int(j[8].replace(",", ""))
+            temp_list.append(temp)
+
+    for i in range(1, 13):
+        data = spark.read.csv("/Housing/data/hadoop_upload/sold_cost/20."+str(i)+".csv", encoding="cp949")
+        data_coll = data.collect()
+        rdr = list()
+
         for j in data_coll:
             rdr.append(j.asDict())
 
-        code_dict = create_code_dict()
-        temp_list = []
-        save_data = {}
-
-        for i in rdr:
+        for j in rdr:
             temp = dict()
-            temp['place'] = i[0]
-            temp['area_grade'] = str(int(float(i[5]) // 10)) + '단위'
+            temp['place'] = j[0]
+            temp['area_grade'] = str(int(float(j[5]) // 10)) + '단위'
             try:
-                temp['area_code'] = code_dict[i[0]]
+                temp['area_code'] = code_dict[j[0]]
             except KeyError:
-                del temp
-            temp['month'] = i[6]
-            temp['cost'] = int(i[8].replace(",",""))
+                continue
+            temp['month'] = j[6]
+            temp['cost'] = int(j[8].replace(",", ""))
             temp_list.append(temp)
-
-        month = temp['month']
-        save_data[month] = temp_list
-
-        with open(str(month)+'.json', 'w', encoding='utf-8') as f1:
-            f1.write(json.dumps(temp_list, ensure_ascii=False))
-
-    for i in range(1, 13):
-        data = spark.read.json("/Housing/data/hadoop_upload/21."+str(i)+".csv", encoding="cp949", header=True)
-        data_coll = data.collect()
-        rdr = list()
-        for j in data_coll:
-            rdr.append(j.asDict())
-
-        code_dict = create_code_dict()
-        temp_list = []
-        save_data = {}
-
-        for i in rdr:
-            temp = dict()
-            temp['place'] = i[0]
-            temp['area_grade'] = str(int(float(i[5]) // 10)) + '단위'
-            try:
-                temp['area_code'] = code_dict[i[0]]
-            except KeyError:
-                del temp
-            temp['month'] = i[6]
-            temp['cost'] = int(i[8].replace(",",""))
-            temp_list.append(temp)
-
-        month = temp['month']
-        save_data[month] = temp_list
-
-        with open(str(month)+'.json', 'w', encoding='utf-8') as f2:
-            f2.write(json.dumps(temp_list, ensure_ascii=False))
 
     for i in range(1, 3):
-        data = spark.read.json("/Housing/data/hadoop_upload/22."+str(i)+".csv", encoding="cp949", header=True)
+        data = spark.read.csv("/Housing/data/hadoop_upload/sold_cost/22." + str(i) + ".csv", encoding="cp949")
         data_coll = data.collect()
         rdr = list()
+
         for j in data_coll:
             rdr.append(j.asDict())
-        code_dict = create_code_dict()
-        temp_list = []
-        save_data = {}
 
-        for i in rdr:
+        for j in rdr:
             temp = dict()
-            temp['place'] = i[0]
-            temp['area_grade'] = str(int(float(i[5]) // 10)) + '단위'
+            temp['place'] = j[0]
+            temp['area_grade'] = str(int(float(j[5]) // 10)) + '단위'
             try:
-                temp['area_code'] = code_dict[i[0]]
+                temp['area_code'] = code_dict[j[0]]
             except KeyError:
-                del temp
-            temp['month'] = i[6]
-            temp['cost'] = int(i[8].replace(",",""))
+                continue
+            temp['month'] = j[6]
+            temp['cost'] = int(j[8].replace(",", ""))
             temp_list.append(temp)
 
-        month = temp['month']
-        save_data[month] = temp_list
+    df_data = spark.createDataFrame(temp_list)
 
-        with open(str(month)+'.json', 'w', encoding='utf-8') as f3:
-            f3.write(json.dumps(temp_list, ensure_ascii=False))
+    user = "root"
+    password = "1234"
+    url = "jdbc:mysql://localhost:3306/Housing"
+    driver = "com.mysql.cj.jdbc.Driver"
+    dbtable = "sold_cost_mean"
+    df_data.write.jdbc(url, dbtable, "append", properties={"driver": driver, "user": user, "password": password})
 
-    data1 = json.load(f1)
-    data2 = json.load(f2)
-    data3 = json.load(f3)
-
-    with open("../data/sold_cost/sold_cost_data.json", "w") as sold_cost_data:
-        json.dump(data1+data2+data3, sold_cost_data, ensure_ascii=False)
 
 if __name__ == "__main__":
     save_json()
