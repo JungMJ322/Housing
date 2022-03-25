6135d732f1116dda937fbf608808f82a33470c57
JungMJ322 <96896783+JungMJ322@users.noreply.github.com>
2022-03-25 15:18:13 +0900

    Update subwayPertreat.py

--- data/change_module/subwayPertreat.py
+++ data/change_module/subwayPertreat.py
@@ -11,7 +11,7 @@ json_name = 'subway2.json'
 
 def subwayPer(location=location, file_name=file_name, json_name=json_name):
 
-    df_json = spark.read.json(f'/Housing/data/hadoop_upload/{file_name}.json')
+    df_json = spark.read.json(f'/Housing/data/hadoop_upload/{file_name}')
     df_json.createOrReplaceTempView('subway')
     df_json2 = spark.sql("""select 역번호 as subway_code, 역사명 as stn_name, 노선명 as route_name, 
                             역위도 as lat, 역경도 as lot 역사 도로명주소 as place form subway""")
@@ -31,7 +31,7 @@ def subwayPer(location=location, file_name=file_name, json_name=json_name):
         data['id'] = cnt
         cnt = cnt + 1
 
-    with open((location + json_name), 'w', encoding='utf8') as f:
+    with open((location + file_name), 'w', encoding='utf8') as f:
         json.dump(json_file['data'], f, ensure_ascii=False)
 
 if __name__ == '__main__':
