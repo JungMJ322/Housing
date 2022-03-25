2c1dd629b927a88fa97f2e0e3f98dc7163e05705
BubDubJP <96857362+BubDubJP@users.noreply.github.com>
2022-03-25 15:35:32 +0900

    change

--- test/test_jp.py
+++ test/test_jp.py
@@ -30,6 +30,5 @@ import requests
 # print(dir(res.request))
 # print(res.request.body)
 
-url = "http://cu.bgfretail.com/store/list_Ajax.do?pageIndex=1&listType=&jumpoCode=&jumpoLotto=&jumpoToto=&jumpoCash=&jumpoHour=&jumpoCafe=&jumpoDelivery=&jumpoBakery=&jumpoFry=&jumpoMultiDevice=&jumpoPosCash=&jumpoBattery=&jumpoAdderss=&jumpoSido=&jumpoGugun=&jumpodong=&user_id=&sido=%EC%84%9C%EC%9A%B8%ED%8A%B9%EB%B3%84%EC%8B%9C&Gugun=&jumpoName="
-res = requests.post(url)
-print(res.text)
+test = "영업/정상"
+print(len(test))
