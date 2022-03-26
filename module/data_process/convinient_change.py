9467bbc3245a99afc14a112faea91c5bfd57de5d
JungMJ322 <96896783+JungMJ322@users.noreply.github.com>
2022-03-26 20:21:52 +0900

    Update convinient_change.py

--- module/data_process/convinient_change.py
+++ module/data_process/convinient_change.py
@@ -42,7 +42,7 @@ def mart_change():
     for i in rdr:
         temp_dict = {}
         try:
-            if ((i["소재지전체주소"] != None) or (i["도로명전체주소"] != None)):
+            if ((i["소재지전체주소"] == None) or (i["도로명전체주소"] == None)):
                 continue
             if (len(i['영업상태명']) != 5) or ((len(i["소재지전체주소"]) < 5) and (len(i["도로명전체주소"]) < 5)):
                 continue
