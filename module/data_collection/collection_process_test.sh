e37cc4164af4df5c0aa4e33cd4e05ac1d82257df
JungMJ322 <96896783+JungMJ322@users.noreply.github.com>
2022-03-26 19:50:32 +0900

    Update
    수정

---
+++ module/data_collection/collection_process_test.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+HERE=$(dirname $(realpath $0))
+echo HERE
+
+hdfs dfs -mkdir /Housing
+sleep 5
+
+hdfs dfs -mkdir /Housing/data
+sleep 5
+
+spark-submit data_collection/data_collection.py
+sleep 5
+
+hdfs dfs -put ../data/hadoop_upload /Housing/data
+sleep 5
+
+spark-submit data_process/data_process.py
+sleep 5
+
+exit 0
