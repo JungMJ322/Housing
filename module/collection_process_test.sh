93a9c13f3c76e8df0b0164d2b3e5eefebe9968b0
JungMJ322 <96896783+JungMJ322@users.noreply.github.com>
2022-03-26 16:50:31 +0900

    Update collection_process_test.sh

--- module/collection_process_test.sh
+++ module/collection_process_test.sh
@@ -3,18 +3,18 @@
 HERE=$(dirname $(realpath $0))
 
 hdfs dfs -mkdir /Housing
-sleep(5)
+sleep 5
 
 hdfs dfs -mkdir /Housing/data
-sleep(5)
+sleep 5
 
 spark-submit HERE/data_collection/data_collection.py
-sleep(5)
+sleep 5
 
 hdfs dfs -put HERE/../data/hadoop_upload /Housing/data
-sleep(5)
+sleep 5
 
 spark-submit HERE/data_process/data_process.py
-sleep(5)
+sleep 5
 
 exit 0
