ced34da94dbab083ff27db0ad0008e6c270ae257
JungMJ322 <96896783+JungMJ322@users.noreply.github.com>
2022-03-26 18:01:38 +0900

    Update collection_process_test.sh

--- module/collection_process_test.sh
+++ module/collection_process_test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-HERE=$(dirname $(dirname $0))
-echo $HERE
+HERE=$(dirname $(realpath $0))
+export HERE
 
 hdfs dfs -mkdir /Housing
 sleep 5
@@ -9,13 +9,13 @@ sleep 5
 hdfs dfs -mkdir /Housing/data
 sleep 5
 
-spark-submit data_collection/data_collection.py
+spark-submit HERE/data_collection/data_collection.py
 sleep 5
 
-hdfs dfs -put ../data/hadoop_upload /Housing/data
+hdfs dfs -put HERE/../data/hadoop_upload /Housing/data
 sleep 5
 
-spark-submit data_process/data_process.py
+spark-submit HERE/data_process/data_process.py
 sleep 5
 
 exit 0
