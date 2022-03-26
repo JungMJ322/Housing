26165f15aece78fc29b6ecd984d9dd18e0b1e846
JungMJ322 <96896783+JungMJ322@users.noreply.github.com>
2022-03-26 19:56:05 +0900

    Update collection_process_test.sh

--- module/collection_process_test.sh
+++ module/collection_process_test.sh
@@ -12,10 +12,10 @@ sleep 5
 spark-submit data_collection/data_collection.py
 sleep 5
 
-hdfs dfs -put ../data/hadoop_upload /Housing/data
+hdfs dfs -put ../../data/hadoop_upload /Housing/data
 sleep 5
 
-spark-submit data_process/data_process.py
+spark-submit ../data_process/data_process.py
 sleep 5
 
 exit 0
