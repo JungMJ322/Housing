96ca86248b97e3ba7d3ce719a7b78584248109bf
JungMJ322 <96896783+JungMJ322@users.noreply.github.com>
2022-03-26 18:25:20 +0900

    Update data_collection.py

--- module/data_collection/data_collection.py
+++ module/data_collection/data_collection.py
@@ -9,10 +9,10 @@ import GS25
 
 
 if __name__ == "__main__":
-    competition_rate.save_data()
-    # cu.js_save()
+        # cu.js_save()
     detailed_inquiry.getDetailedAPI()
     detailed_inquiry.append_location()
+    competition_rate.save_data()
     # getHospital.getHospital()
     # GS25.data_save(GS25.crawling())
     sevenEleven.data_save()
