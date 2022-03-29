0133cdb209942f4b92cca7bf8ee7700a1c930706
BubDubJP <96857362+BubDubJP@users.noreply.github.com>
2022-03-29 10:13:46 +0900

    Update views.py

--- django/housing/housing/views.py
+++ django/housing/housing/views.py
@@ -2,25 +2,38 @@ from django.shortcuts import render, redirect
 from .models import Busstop, SoldCostMean, Detail, Hospital, Infra, Mart, Park, School, Subway
 
 
-def extract(key_list, data):
+def extract_ByKeys(key_list, object_name):
     return_dict = {}
-    count=0
-    for j in key_list:
-        return_dict[j] = []
-
-    for i in data:
-        if count > 100:
-            break
-        for j in key_list:
-            return_dict[j].append(i[j])
-        count+=1
-
+    for i in key_list:
+        temp_list = object_name.objects.values_list(i)
+        return_dict[i] = temp_list
     return return_dict
+
+    # return_dict = {}
+    # count=0
+    # for j in key_list:
+    #     return_dict[j] = []
+    #
+    # for i in data:
+    #     if count > 100:
+    #         break
+    #     for j in key_list:
+    #         return_dict[j].append(i[j])
+    #     count += 1
+
+
+def all_object_call(object_name):
+    return object_name.objects.all().values()
+
+
+# def extract_ByFilter(dict_list):
+#     key_list = dict_list.keys()
+#     value_list = dict_list.values()
+#     for i in range(len(key_list)):
 
 
 def index(request):
-    stops = SoldCostMean.objects.all().values()
-    output = extract(['place_code', 'area_grade'], stops)
+    output = SoldCostMean.objects.filter(area_grade='3단위').filter(mean_cost=5200.0).values()
     print(output)
 
     return render(request, 'index.html')
