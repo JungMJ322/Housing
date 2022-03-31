c35ff0b6406e8d13fe8b4cd38f18d33ccb76b922
sol <soloolol@naver.com>
2022-03-31 14:05:34 +0900

    render rank
    -

--- djangof/housing/housing/views.py
+++ djangof/housing/housing/views.py
@@ -409,31 +409,40 @@ def getRateSido(sido):
 
     return result
 
-def rankCompet(sido):
+def rankCompet(request, max_count=8):
+    sido = request.GET['sido']
+
     cursor = connection.cursor()
 
-    strSql = f"""select dense_rank() over(order by compet_rate desc), house_name, compet_rate
+    strSql = f"""select dense_rank() over(order by compet_rate desc) as rank, house_name, compet_rate
                 from competition join detail on (competition.house_manage_no = detail.house_manage_no)
-                where address like '{sido}%' and compet_rate != 'lacked'"""
+                where address like '{sido}%' and compet_rate != 'lacked' limit {max_count}"""
     success = cursor.execute(strSql)
     rank_all = list(cursor.fetchall())
     temp1=[]
     rank_cut=5
-    max_count=10
-    for i in range(max_count):
+    for r in rank_all:
         temp2 = {}
-        if rank_all[i][0] <= rank_cut:
-            temp2['rank']=rank_all[i][0]
-            temp2['name']=rank_all[i][1]
-            temp2['compet']=rank_all[i][2]
+        if r[0] <= rank_cut:
+            temp2['rank']=r[0]
+            temp2['name']=r[1]
+            temp2['compet']=r[2]
             temp1.append(temp2)
     result=temp1
 
-    return result
+    # render 로 보내려고 스트링 작업
+    rank_5=[]
+    for i in result:
+        temp3= i['rank']+i['name']+i['compet']
+        rank_5.append(temp3)
+    context = {
+        'rank1':rank_5[0],'rank2':rank_5[1], 'rank3':rank_5[2], 'rank4':rank_5[3], 'rank5':rank_5[4]
+    }
+    return render(request, 'city.html', context)
 
 def index(request):
-    temp= rankCompet("대구")
-    print(temp)
+    #temp= rankCompet("대구")
+    #print(temp)
     return render(request, 'index.html')
 
 
